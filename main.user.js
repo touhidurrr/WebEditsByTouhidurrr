62907e837377ced21b7750793a4cf75db0d3cd6d
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2021-06-07 04:52:10 +0600

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -2,31 +2,32 @@
 // @name            WebEditsByTouhidurrr
 // @name:en         WebEditsByTouhidurrr
 // @namespace       http://tampermonkey.net/
-// @version         0.0.13
+// @version         0.0.14
 // @description     WebEditsByTouhidurrr is an UserScript written in Javascript that does nothing but fixing the web as touhidurrr likes. Simply it's my personal UserScript.
 // @description:en  WebEditsByTouhidurrr is an UserScript written in Javascript that does nothing but fixing the web as touhidurrr likes. Simply it's my personal UserScript.
 // @author          touhidurrr
 // @homepageURL     https://touhidur.xyz/
-// @updateURL       https://github.com/touhidurrr/WebEditsByTouhidurrr/raw/main/main.meta.js
+// @updateURL       https://github.com/touhidurrr/WebEditsByTouhidurrr/raw/main/main.user.js
 // @downloadURL     https://github.com/touhidurrr/WebEditsByTouhidurrr/raw/main/main.user.js
 // @match           http*://*.wordpress.com/*
 // @icon            https://touhidur.xyz/favicon.png
 // @grant           none
 // ==/UserScript==
 
-'use strict';
-
 Element.prototype.remove = function() {
-   this.parentElement.removeChild(this);
+  this.parentElement.removeChild(this);
 }
 
 NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
   for(var i = this.length - 1; i >= 0; i--) {
-      if(this[i] && this[i].parentElement) {
-          this[i].parentElement.removeChild(this[i]);
-      }
+    if(this[i] && this[i].parentElement) {
+      this[i].parentElement.removeChild(this[i]);
+    }
   }
 }
 
-document.getElementsByTagName('script').remove();
-document.getElementById('marketingbar').remove();
+(function() {
+  'use strict';
+  document.getElementsByTagName('script').remove();
+  document.getElementById('marketingbar').remove();
+})();
