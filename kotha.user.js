ef5e59c1d45dae14b3d74bb263ad8c8f5307daa6
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2021-06-08 05:25:13 +0600

    Add files via upload

---
+++ kotha.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name            kotha
+// @version         0.0.1
+// @author          touhidurrr
+// @description     An UserScript that I made for my friend to highlight the name of theperson he likes.
+// @run-at          document-end
+// @namespace       http://tampermonkey.net/
+// @icon            https://touhidur.xyz/favicon.png
+// @homepageURL     https://touhidur.xyz/
+// @updateURL       https://github.com/touhidurrr/WebEditsByTouhidurrr/raw/main/kotha.user.js
+// @downloadURL     https://github.com/touhidurrr/WebEditsByTouhidurrr/raw/main/kotha.user.js
+// @grant           none
+// @match           *
+// ==/UserScript==
+
+(function() {
+  function replaceInText(element, pattern, replacement) {
+    for (let node of element.childNodes) {
+      switch (node.nodeType) {
+        case Node.ELEMENT_NODE:
+          replaceInText(node, pattern, replacement);
+          break;
+        case Node.TEXT_NODE:
+          node.textContent = node.textContent.replace(pattern, replacement);
+          break;
+        case Node.DOCUMENT_NODE:
+          replaceInText(node, pattern, replacement);
+      }
+    }
+  }
+  replaceInText(document.body, /(kotha)/ig, '<div style="color:black;background:yellow;">' + /$1/ + /<\/div>/);
+})();
