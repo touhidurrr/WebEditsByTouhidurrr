2ae6f2eed9a6e3bc18572ef13fe7ac127ffa5674
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2021-06-08 20:58:36 +0600

    Update kotha.user.js

--- kotha.user.js
+++ kotha.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            kotha
-// @version         0.0.22
+// @version         0.0.23
 // @author          touhidurrr
 // @description     An UserScript that I made for my friend to highlight the name of theperson he likes.
 // @run-at          document-end
@@ -10,10 +10,15 @@
 // @updateURL       https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/kotha.user.js
 // @downloadURL     https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/kotha.user.js
 // @grant           none
-// @match           https://*
-// @match           http://*
+// @include         *
 // ==/UserScript==
 
-document.body.innerHTML = document.body.innerHTML.replace(/kotha/gi, function(s){
-  return '<font style="color:black;background:yellow;">' + s + '</font>';
-});
+(function(){
+  for (let node=document.body;node;node=node.nextSibling){
+    if (node.nodeType==1) {
+      node.innerHTML = node.innerHTML.replace(/kotha/gi, function(s) {
+       return '<font style="color:black;background:yellow;">' + s + '</font>';
+      });
+    }
+  }
+})();
