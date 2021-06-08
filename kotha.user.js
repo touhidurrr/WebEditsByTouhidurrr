5f1b3faf9fa9f860033c1dd2ee0ddee8e6af4985
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2021-06-08 06:00:22 +0000

    Create kotha.user.js

--- kotha.user.js
+++ kotha.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            kotha
-// @version         0.0.14
+// @version         0.0.15
 // @author          touhidurrr
 // @description     An UserScript that I made for my friend to highlight the name of theperson he likes.
 // @run-at          document-idle
