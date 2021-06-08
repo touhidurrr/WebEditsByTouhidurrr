9cc8e370b2cd7705aba00d9f5f95295b1a3821ca
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2021-06-08 05:55:12 +0000

    Create kotha.user.js

--- kotha.user.js
+++ kotha.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name            kotha
-// @version         0.0.12
+// @version         0.0.13
 // @author          touhidurrr
 // @description     An UserScript that I made for my friend to highlight the name of theperson he likes.
 // @run-at          document-idle
 // @namespace       http://tampermonkey.net/
 // @icon            https://touhidur.xyz/favicon.png
 // @homepageURL     https://touhidur.xyz/
-// @updateURL       https://github.com/touhidurrr/WebEditsByTouhidurrr/raw/main/kotha.user.js
-// @downloadURL     https://github.com/touhidurrr/WebEditsByTouhidurrr/raw/main/kotha.user.js
+// @updateURL       https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/kotha.user.js
+// @downloadURL     https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/kotha.user.js
 // @grant           none
 // @match           http*://*
 // ==/UserScript==
