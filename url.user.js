cb98c7f8f070772c82b63ad3b28096980b358345
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2021-06-10 02:46:23 +0600

    Update url.user.js

--- url.user.js
+++ url.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            URL-touhidurrr
 // @name:en         URL-touhidurrr
-// @version         0.0.4
+// @version         0.0.5
 // @author          touhidurrr
 // @description     An URL redirection Manager for touhidurrr
 // @description:en  An URL redirection Manager for touhidurrr
