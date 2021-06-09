35b0ec8037f8089309bf33b430258e840a9cb6ac
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2021-06-10 01:51:56 +0600

    Create meta_generator.py

---
+++ .github/workflows/meta_generator.py
@@ -0,0 +1,19 @@
+import glob
+
+fileList = glob.glob("*.user.js")
+
+for path in fileList:
+  meta = ''
+  
+  with open(path, mode = 'r') as f:
+    
+    for line in f.readlines():
+      meta += line
+      
+    if line == '// ==/UserScript==\n':
+      break
+  
+  metaPath = './meta/' + path[:-8] + '.meta.js'
+  with open(metaPath, mode = 'w') as f:
+    
+    f.write(meta)
