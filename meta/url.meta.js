e768c8306554869b24f54bbf0ea1595f27e4b87b
touhidurrr <46617994+touhidurrr@users.noreply.github.com>
2021-06-09 20:41:21 +0000

    Generated Meta

---
+++ meta/url.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name            URL-touhidurrr
+// @name:en         URL-touhidurrr
+// @version         0.0.4
+// @author          touhidurrr
+// @description     An URL redirection Manager for touhidurrr
+// @description:en  An URL redirection Manager for touhidurrr
+// @run-at          document-start
+// @namespace       http://tampermonkey.net/
+// @icon            https://touhidur.xyz/favicon.png
+// @homepageURL     https://touhidur.xyz/
+// @updateURL       https://github.com/touhidurrr/WebEditsByTouhidurrr/raw/main/url.user.js
+// @downloadURL     https://github.com/touhidurrr/WebEditsByTouhidurrr/raw/main/url.user.js
+// @grant           none
+// @include         http://*
+// ==/UserScript==
---
+++ meta/webedits.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name            WebEditsByTouhidurrr
+// @name:en         WebEditsByTouhidurrr
+// @version         0.0.18
+// @author          touhidurrr
+// @description     WebEditsByTouhidurrr is an UserScript written in Javascript that does nothing but fixing the web as touhidurrr likes. Simply it's my personal UserScript.
+// @description:en  WebEditsByTouhidurrr is an UserScript written in Javascript that does nothing but fixing the web as touhidurrr likes. Simply it's my personal UserScript.
+// @run-at          document-end
+// @namespace       http://tampermonkey.net/
+// @icon            https://touhidur.xyz/favicon.png
+// @homepageURL     https://touhidur.xyz/
+// @updateURL       https://github.com/touhidurrr/WebEditsByTouhidurrr/raw/main/webedits.user.js
+// @downloadURL     https://github.com/touhidurrr/WebEditsByTouhidurrr/raw/main/webedits.user.js
+// @grant           none
+// @match           https://*.wordpress.com/*
+// ==/UserScript==
