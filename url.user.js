6858862a878cf6ae3798182e7fcd29efe0a0b2b6
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2021-06-10 18:30:32 +0600

    Update url.user.js

--- url.user.js
+++ url.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            URL-touhidurrr
 // @name:en         URL-touhidurrr
-// @version         0.0.6
+// @version         0.0.7
 // @author          touhidurrr
 // @description     An URL redirection Manager for touhidurrr
 // @description:en  An URL redirection Manager for touhidurrr
@@ -9,8 +9,8 @@
 // @namespace       http://tampermonkey.net/
 // @icon            https://touhidur.xyz/favicon.png
 // @homepageURL     https://touhidur.xyz/
-// @updateURL       https://github.com/touhidurrr/WebEditsByTouhidurrr/raw/main/meta/url.meta.js
-// @downloadURL     https://github.com/touhidurrr/WebEditsByTouhidurrr/raw/main/url.user.js
+// @updateURL       https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/meta/url.meta.js
+// @downloadURL     https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/url.user.js
 // @grant           none
 // @include         http://*
 // ==/UserScript==
