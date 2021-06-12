a99d8bd9a3f903db7f69b232e7494b4cab9ed060
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2021-06-13 05:34:53 +0600

    Update url.user.js

--- url.user.js
+++ url.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            URL-touhidurrr
 // @name:en         URL-touhidurrr
-// @version         0.0.9
+// @version         0.0.10
 // @author          touhidurrr
 // @description     An URL redirection Manager for touhidurrr
 // @description:en  An URL redirection Manager for touhidurrr
@@ -15,18 +15,29 @@
 // @include         http*
 // ==/UserScript==
 
-if (window.location.href.startsWith('http://')) (function() {
+(function() {
   let excludeList = [
     'ugadmission.buet.ac.bd',
-    'uga1.buet.ac.bd'
+    'uga1.buet.ac.bd',
+    'touhidur',
+    'localhost'
   ];
-  if (!excludeList.includes(window.location.hostname)) window.location.replace('https' + window.location.href.slice(4));
-})();
-else if (window.location.href.startsWith('https://')) {
-  document.addEventListener("DOMContentLoaded", function(e) {
-    let links = document.getElementsByTagName('a');
-    for (let link of links) {
-      if (link.href.startsWith('http://')) link.href = 'https' + link.href.slice(4);
+
+  if (window.location.href.startsWith('http://')) {
+    if (!excludeList.includes(window.location.hostname)) {
+      window.location.replace('https' + window.location.href.slice(4));
     }
-  });
-}
+  }
+
+  else if (window.location.href.startsWith('https://')) {
+    document.addEventListener("DOMContentLoaded", function(e) {
+      let links = document.getElementsByTagName('a');
+      for (let link of links) {
+        if (link.href.startsWith('http://')) {
+            let ln = link.href.slice(4);
+            if (!excludeList.includes(ln.split('/')[0])) link.href = 'https' + ln;
+        }
+      }
+    });
+  }
+})();
