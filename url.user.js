22414b6693a3ad37b20ab7f57b94d8e815a8e122
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2021-06-12 20:03:17 +0600

    Update url.user.js

--- url.user.js
+++ url.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            URL-touhidurrr
 // @name:en         URL-touhidurrr
-// @version         0.0.7
+// @version         0.0.8
 // @author          touhidurrr
 // @description     An URL redirection Manager for touhidurrr
 // @description:en  An URL redirection Manager for touhidurrr
@@ -12,13 +12,21 @@
 // @updateURL       https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/meta/url.meta.js
 // @downloadURL     https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/url.user.js
 // @grant           none
-// @include         http://*
+// @include         *
 // ==/UserScript==
 
-(function() {
+if (window.location.href.startsWith('http://')) (function() {
   let excludeList = [
     'ugadmission.buet.ac.bd',
     'uga1.buet.ac.bd'
   ];
   if (!excludeList.includes(window.location.hostname)) window.location.replace('https' + window.location.href.slice(4));
+})();
+else if (window.location.href.startsWith('https://')) (function() {
+    document.addEventListener("DOMContentLoaded", function(event) {
+    let links = document.getElementsByTagName('a');
+    for (let link of links) {
+      if (link.href.startsWith('http://')) link.href = 'https' + link.href.slice(4);
+    }
+  });
 })();
