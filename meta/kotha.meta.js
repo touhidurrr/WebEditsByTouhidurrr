947d95a4cdbd68750d100682a0026f9ff7689dfc
touhidurrr <46617994+touhidurrr@users.noreply.github.com>
2021-06-13 00:46:36 +0000

    Generated Meta

--- meta/kotha.meta.js
+++ meta/kotha.meta.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name            kotha
 // @version         0.0.27
-// @author          touhidurrr
 // @description     An UserScript that I made for my friend to highlight the name of theperson he likes.
+// @author          touhidurrr
 // @run-at          document-end
-// @namespace       http://tampermonkey.net/
 // @icon            https://touhidur.xyz/favicon.png
+// @namespace       https://github.com/touhidurrr
 // @homepageURL     https://touhidur.xyz/
+// @supportURL      https://github.com/touhidurrr/WebEditsByTouhidurrr/issues
 // @updateURL       https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/meta/kotha.meta.js
 // @downloadURL     https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/kotha.user.js
 // @grant           none
