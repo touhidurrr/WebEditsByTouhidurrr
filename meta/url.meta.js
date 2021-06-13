135c3291bbcb51be21b80736214960fc1b298b3b
touhidurrr <46617994+touhidurrr@users.noreply.github.com>
2021-06-13 00:41:32 +0000

    Generated Meta

--- meta/url.meta.js
+++ meta/url.meta.js
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @name            URL-touhidurrr
 // @name:en         URL-touhidurrr
-// @version         0.0.11
-// @author          touhidurrr
+// @version         0.0.12
 // @description     An URL redirection Manager for touhidurrr
 // @description:en  An URL redirection Manager for touhidurrr
+// @author          touhidurrr
 // @run-at          document-start
-// @namespace       http://tampermonkey.net/
 // @icon            https://touhidur.xyz/favicon.png
+// @namespace       https://github.com/touhidurrr
 // @homepageURL     https://touhidur.xyz/
+// @supportURL      https://github.com/touhidurrr/WebEditsByTouhidurrr/issues
 // @updateURL       https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/meta/url.meta.js
 // @downloadURL     https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/url.user.js
 // @grant           none
-// @include         http*
+// @include         *
 // ==/UserScript==
