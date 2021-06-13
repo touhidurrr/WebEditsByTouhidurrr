ecc9ef5e008b27b667a6497610c5bbca0e997d7f
touhidurrr <46617994+touhidurrr@users.noreply.github.com>
2021-06-13 17:23:44 +0000

    Generated Meta

---
+++ meta/relative.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name            Relative-touhidurrr
+// @name:en         Relative-touhidurrr
+// @version         0.0.1
+// @description     A tools to change absolute urls to relative urls during accessing html files from device Storage.
+// @description:en  A tools to change absolute urls to relative urls during accessing html files from device Storage.
+// @author          touhidurrr
+// @run-at          document-end
+// @icon            https://touhidur.xyz/favicon.png
+// @namespace       https://github.com/touhidurrr
+// @homepageURL     https://touhidur.xyz/
+// @supportURL      https://github.com/touhidurrr/WebEditsByTouhidurrr/issues
+// @updateURL       https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/meta/relative.meta.js
+// @downloadURL     https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/relative.user.js
+// @grant           none
+// @include         file://*
+// ==/UserScript==
