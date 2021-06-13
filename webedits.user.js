bfac2af9c641f71ac9c52aa2fcf95a679a415b5a
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2021-06-13 06:44:12 +0600

    Update webedits.user.js

--- webedits.user.js
+++ webedits.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name            WebEditsByTouhidurrr
 // @name:en         WebEditsByTouhidurrr
-// @version         0.0.21
-// @author          touhidurrr
+// @version         0.0.22
 // @description     WebEditsByTouhidurrr is an UserScript written in Javascript that does nothing but fixing the web as touhidurrr likes. Simply it's my personal UserScript.
 // @description:en  WebEditsByTouhidurrr is an UserScript written in Javascript that does nothing but fixing the web as touhidurrr likes. Simply it's my personal UserScript.
+// @author          touhidurrr
 // @run-at          document-end
-// @namespace       http://tampermonkey.net/
 // @icon            https://touhidur.xyz/favicon.png
+// @namespace       https://github.com/touhidurrr
 // @homepageURL     https://touhidur.xyz/
+// @supportURL      https://github.com/touhidurrr/WebEditsByTouhidurrr/issues
 // @updateURL       https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/meta/webedits.meta.js
 // @downloadURL     https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/webedits.user.js
 // @grant           none
