d5b24b095ff330280eb4b2f6ac2412ed912b6f54
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2021-06-13 06:46:23 +0600

    Update kotha.user.js

--- kotha.user.js
+++ kotha.user.js
@@ -1,14 +1,33 @@
 // ==UserScript==
 // @name            kotha
 // @version         0.0.27
-// @author          touhidurrr
 // @description     An UserScript that I made for my friend to highlight the name of theperson he likes.
+// @author          touhidurrr
 // @run-at          document-end
-// @namespace       http://tampermonkey.net/
 // @icon            https://touhidur.xyz/favicon.png
+// @namespace       https://github.com/touhidurrr
 // @homepageURL     https://touhidur.xyz/
+// @supportURL      https://github.com/touhidurrr/WebEditsByTouhidurrr/issues
 // @updateURL       https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/meta/kotha.meta.js
 // @downloadURL     https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/kotha.user.js
+// @grant           none
+// @include         *
+// ==/UserScript==
+
+// ==UserScript==
+// @name            URL-touhidurrr
+// @name:en         URL-touhidurrr
+// @version         0.0.12
+// @description     An URL redirection Manager for touhidurrr
+// @description:en  An URL redirection Manager for touhidurrr
+// @author          touhidurrr
+// @run-at          document-start
+// @icon            https://touhidur.xyz/favicon.png
+// @namespace       https://github.com/touhidurrr
+// @homepageURL     https://touhidur.xyz/
+// @supportURL      https://github.com/touhidurrr/WebEditsByTouhidurrr/issues
+// @updateURL       https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/meta/url.meta.js
+// @downloadURL     https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/url.user.js
 // @grant           none
 // @include         *
 // ==/UserScript==
@@ -17,7 +36,7 @@
   for (let node=document.body;node;node=node.nextSibling){
     if (node.nodeType==1) {
       node.innerHTML = node.innerHTML.replace(/kotha/gi, function(s) {
-       return '<span style="color:black;background:yellow;">' + s + '</span>';
+        return '<span style="color:black;background:yellow;">' + s + '</span>';
       });
     }
   }
