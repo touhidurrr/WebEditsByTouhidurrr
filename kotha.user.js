970bd44da54dc3a24f6ead795895ccd62ff5f04d
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2021-06-13 06:48:39 +0600

    Update kotha.user.js

--- kotha.user.js
+++ kotha.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            kotha
-// @version         0.0.27
+// @version         0.0.28
 // @description     An UserScript that I made for my friend to highlight the name of theperson he likes.
 // @author          touhidurrr
 // @run-at          document-end
@@ -10,24 +10,6 @@
 // @supportURL      https://github.com/touhidurrr/WebEditsByTouhidurrr/issues
 // @updateURL       https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/meta/kotha.meta.js
 // @downloadURL     https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/kotha.user.js
-// @grant           none
-// @include         *
-// ==/UserScript==
-
-// ==UserScript==
-// @name            URL-touhidurrr
-// @name:en         URL-touhidurrr
-// @version         0.0.12
-// @description     An URL redirection Manager for touhidurrr
-// @description:en  An URL redirection Manager for touhidurrr
-// @author          touhidurrr
-// @run-at          document-start
-// @icon            https://touhidur.xyz/favicon.png
-// @namespace       https://github.com/touhidurrr
-// @homepageURL     https://touhidur.xyz/
-// @supportURL      https://github.com/touhidurrr/WebEditsByTouhidurrr/issues
-// @updateURL       https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/meta/url.meta.js
-// @downloadURL     https://raw.githubusercontent.com/touhidurrr/WebEditsByTouhidurrr/main/url.user.js
 // @grant           none
 // @include         *
 // ==/UserScript==
