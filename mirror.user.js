3a07658b61fad60281f6c5405ae2b4e5a186f27c
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2021-09-01 23:29:33 +0600

    Update mirror.user.js

--- mirror.user.js
+++ mirror.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name            mirrorTouhidurrr
-// @version         0.0.1
+// @version         0.0.2
 // @description     redirects traffic through mirror.touhidur.xyz
 // @author          touhidurrr
 // @run-at          document-start
 // @icon            https://touhidur.xyz/favicon.png
 // @homepageURL     https://touhidur.xyz/
 // @grant           none
-// @include         *
+// @include         https://*
 // ==/UserScript==
 
 (function () {
